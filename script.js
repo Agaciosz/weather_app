fbd2e93674caec1f8c1628cc675588de45ac8d86
Agaciosz <agata.ewa.glab@gmail.com>
2023-10-29 09:34:26 +0100

    Modifying the variables: time, wind, humidity, description

--- script.js
+++ script.js
@@ -13,11 +13,18 @@ let days = [
 let day = days[now.getDay()];
 document.querySelector("#day").innerHTML = day;
 
-let time = [now.getHours(), now.getMinutes()];
-let formattedTime = time.join(":");
-document.querySelector("#time").innerHTML = formattedTime;
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 
-//------------------------------//
+document.querySelector("#time").innerHTML = `${hours}:${minutes}`;
+
+// Show temperature - Share button
 
 function displayCity(event) {
   event.preventDefault();
@@ -34,7 +41,16 @@ function displayTemperature(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(function (response) {
     let temperature = document.querySelector("#temperature");
+    let wind = document.querySelector("#wind");
+    let humidity = document.querySelector("#humidity");
+    let description = document.querySelector("#description");
+    //let time = document.querySelector("#time");
     temperature.innerHTML = Math.round(response.data.main.temp);
+    wind.innerHTML = Math.round(response.data.wind.speed * 3.6);
+    humidity.innerHTML = response.data.main.humidity;
+    description.innerHTML =
+      response.data.weather[0].description.charAt(0).toUpperCase() +
+      response.data.weather[0].description.slice(1);
   });
 }
 
@@ -42,7 +58,7 @@ let form = document.querySelector("form");
 form.addEventListener("submit", displayCity);
 form.addEventListener("click", displayCity);
 
-//------------------------------//
+// Show current temperature
 
 function showPosition(position) {
   let latitude = position.coords.latitude;
@@ -58,6 +74,8 @@ function showTemperatureOfMyCity(latitude, longitude) {
     myCity.innerHTML = response.data.name;
     let temperature = document.querySelector("#temperature");
     temperature.innerHTML = Math.round(response.data.main.temp);
+    temperature.innerHTML = Math.round(response.data.main.temp);
+    humidity.innerHTML = response.data.main.humidity;
   });
 }
 
